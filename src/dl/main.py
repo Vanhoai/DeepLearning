24f0eaef6e87222cae9ab24d0edb230f8bb3a85b
Hinsun <vanhoai.adv@gmail.com>
2025-06-18 21:40:48 +0700

    👀 feat: add mobilenet testing in notebook but not done

--- src/dl/main.py
+++ src/dl/main.py
@@ -1,48 +1,37 @@
-import os
-import numpy as np
-import cv2 as cv
-from numpy.typing import NDArray
+import torch
+import torch.nn as nn
+from torchinfo import summary
 
 
-def pad_image_2d(image, padding: int):
-    if padding <= 0:
-        return image
+class SimpleModel(nn.Module):
+    def __init__(self, in_channels=3, num_classes=10):
+        super(SimpleModel, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-    padded = np.pad(image, ((padding, padding), (padding, padding), (0, 0)), mode="constant")
-    return padded
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
 
+        self.fc1 = nn.Linear(32 * 7 * 7, num_classes)
+        self.relu = nn.ReLU(inplace=True)
 
-def conv2d(image: NDArray, kernels: NDArray, stride: int, padding: int):
-    H, W, C = image.shape
-    K, F, _, D = kernels.shape
-    assert C == D, "Image channels must match kernel depth"
-
-    padded = pad_image_2d(image, padding)
-    HO = (H - F + 2 * padding) // stride + 1
-    WO = (W - F + 2 * padding) // stride + 1
-
-    O = np.zeros((HO, WO, K), dtype=np.float32)
-    for k in range(K):
-        for i in range(HO):
-            for j in range(WO):
-                region = padded[i * stride:i * stride + F, j * stride:j * stride + F, :]
-                s = np.sum(region * kernels[k])
-                O[i, j, k] = s
-
-    return O
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        return x
 
 
 def main():
-    root = os.getcwd()
-
-    path = os.path.join(root, "assets", "man.jpg")
-    img = cv.imread(path, cv.COLOR_BGR2RGB)
-    if img is None:
-        raise FileNotFoundError(f"Image not found at {path}")
-
-    # 3 Kernels, with shape (3, 3, 3)
-    kernels = np.random.rand(3, 3, 3, 3)
-    stride = 1
-    padding = 1
-    output = conv2d(img, kernels, stride, padding)
-    print(f"Output shape: {output.shape}")
+    model = SimpleModel()
+    summary(model, input_size=(1, 3, 28, 28))
